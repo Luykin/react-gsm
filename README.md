497df87c1dee901b1f7bdf6b21e7ed3e36a4f54d
lyk@prismslight.com <530366891@qq.com>
2020-12-30 11:22:15 +0800

    init

---
+++ README.md
@@ -0,0 +1,64 @@
+## 🚀react-gsm是一个轻量级的全局状态管理库
+
+##### **⭐️安装**
+
+`yarn add react-gsm`
+
+
+
+##### **⭐️初始化**
+
+请尽可能提前初始化,react-gsm会在浏览器localStorage中主动取出键值对存储全局变量中
+
+```
+import {initGlobal} from 'react-gsm';
+
+const defaultGlobal = {  userInfo: null,  token: null };
+
+const AsyncStorageListKey = ['token', 'userInfo'];
+
+initGlobal(defaultGlobal, AsyncStorageListKey);
+
+```
+
+##### **⭐️简单使用**
+
+bindData会监听全局变量的改变,从而改变每一个页面state中的值,第二参数传入this   bindData(key, local)
+
+setGlobal修改全局变量的值 setGlobal(key, value, callback)
+
+getGlobal获取全局变量的值  getGlobal(key)
+
+```
+export default class Test extends React.Component {  
+    constructor(props) {  
+        super(props);
+        this.state = {  
+            userInfo: bindData('userInfo', this), 
+        }
+    }
+    componentDidMount() {
+         setTimeout(() => {
+            setGlobal('userInfo', {'name': 'luoyukun'}, ()=> {
+                console.log(this.state.userInfo);
+            })
+         }, 1000)
+    }
+}
+```
+
+##### **⭐️localStorage联合使用**
+
+场景:需要缓存用户信息到网页本地,并随时刷新用户信息,在第一次网络请求还没有拿到用户信息的时候能够展示用户上一次缓存的用户信息
+
+setGlobalStorage(key, value, otherKey)
+
+
+
+```
+setGlobalStorage('userInfo', {'name': 'luoyukun'});
+```
+
+##### **⭐️待完善**
+
+
---
+++ global.js
@@ -0,0 +1,103 @@
+const localObj = {};
+let global = null;
+
+function proxy(key) {
+    if (global && (key in global)) {
+        let val = global[key];
+        Object.defineProperty(global, key, {
+            get: function () {
+                return val;
+            },
+            set: function (newValue) {
+                try {
+                    if (newValue !== val) {
+                        if (localObj[key]) {
+                            localObj[key].forEach((local) => {
+                                if (local.setState) {
+                                    local.setState({
+                                        [key]: newValue,
+                                    });
+                                }
+                            });
+                        }
+                        val = newValue;
+                        this[key] = newValue;
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            },
+        });
+    }
+}
+
+function clearLocal(local) {
+    try {
+        const componentWillUnmount = local.componentWillUnmount ? local.componentWillUnmount : () => {
+        };
+        local.componentWillUnmount = () => {
+            componentWillUnmount.call(local);
+            local.setState = () => {
+                return null;
+            };
+        };
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export function bindData(key, local) {
+    if (global && (key in global)) {
+        // eslint-disable-next-line no-unsafe-negation
+        if (!localObj[key] || !localObj[key] instanceof Array) {
+            localObj[key] = [local];
+            clearLocal(local);
+        } else {
+            localObj[key].push(local);
+        }
+        proxy(key);
+        return global[key];
+    } else {
+        console.log(`${key} key not in global`);
+        return {};
+    }
+}
+
+export function setGlobal(key, value, callback) {
+    if (global && (key in global)) {
+        console.log(`全局设置${key},${JSON.stringify(value)} `);
+        global[key] = value;
+        if (callback) {
+            callback();
+        }
+    } else {
+        console.log(`${key} key not in global`);
+    }
+}
+
+export function getGlobal(key) {
+    if (global && (key in global)) {
+        return global[key];
+    } else {
+        console.log(`${key} key not in global`);
+        return {};
+    }
+}
+
+export function setDefaultGlobal(value) {
+    if (!global) {
+        // eslint-disable-next-line no-const-assign
+        global = value;
+    }
+}
+
+export function setGlobalStorage(key, value, otherKey) {
+    try {
+        if ((typeof key === 'string')) {
+            setGlobal(key, value);
+            localStorage.setItem(otherKey || key, JSON.stringify(value));
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
